81a31833a427bc9020d3f15a972b4f0a9a5171e1
samwang1 <samuelw2000@gmail.com>
2020-06-17 16:48:32 -0700

    update sample insert code.sql

--- Sample insert code.sql
+++ Sample insert code.sql
@@ -1,53 +1,56 @@
 USE DOGPAWS_Surveys_Temp
 
 SELECT * FROM WK_1
+select * from RAW_Questions
+
+INSERT INTO tblSURVEY(SurveyName, SurveyBeginDate)
+VALUES('DogPaws Interest Survey', 'May 22, 2020')
+
 
 --
 
 GO
 CREATE PROC uspInsertFromCSV
-@SurveyName varchar(50),
+@SurveyName varchar(100),
 @LastPull DateTime -- time of most recent .csv download
 AS
 BEGIN
 	DECLARE @RowNum INT = 1
-	DECLARE @TotalRows INT = 0
+	DECLARE @TotalRows INT = (SELECT COUNT(*) FROM WK_1)
+	DECLARE @F varchar(50), @L varchar(50), @Email varchar(100), @Temp varchar(100)
 
-	SELECT @TotalRows = COUNT(0) FROM WK_1
+	DECLARE @ResponseDateTime DateTime = (Select Question_1 From WK_1 Where ResponseID = @RowNum),
+		@PersonPK INT
+
+	DECLARE @SurveyID INT = (SELECT SurveyID FROM tblSURVEY WHERE SurveyName = 'DogPaws Interest Survey')
+
 	WHILE @RowNum <= @TotalRows
 	BEGIN
-		-- select current row
-		DECLARE @F varchar(20), @L varchar(20), @Email varchar(50), @temp varchar(500)
-		SET @temp = (SELECT Question_33 FROM WK_1 WHERE ResponseID = @RowNum)-- split by space. sample output: 'Harry Husky'
-		--SET @temp = REPLACE(@temp, ' ', '.') -- Harry.Husky
-		IF (SELECT PARSENAME(@temp, 2)) IS NULL
+
+		SET @Temp = (SELECT Question_33 FROM WK_1 WHERE ResponseID = @RowNum)
+
+		IF @Temp IS NULL OR @Temp = 'N/A'
 			BEGIN
-				SET @F = NULL -- or anonymous
+				SET @F = NULL -- anonymous
+				SET @L = NULL -- anonymous
 			END
 		ELSE
 			BEGIN
-				SET @F = (SELECT PARSENAME(@temp, 2))
+				SET @F = SUBSTRING(@Temp, 1, CHARINDEX(' ', @Temp) - 1)
+				SET @L = SUBSTRING(@Temp, CHARINDEX(' ', @Temp) + 1, LEN(@Temp))
 			END
-		IF (SELECT PARSENAME(@temp, 1)) IS NULL
-			BEGIN
-				SET @L = NULL -- or anonymous
-			END
-		ELSE
-			BEGIN
-				SET @L = (SELECT PARSENAME(@temp, 1))
-			END
-		SET @Email = (SELECT Question_2 /*from current row*/)
+		SET @Email = (SELECT Question_2 FROM WK_1 WHERE ResponseID = @RowNum)
 
+		IF NOT EXISTS (SELECT PersonID FROM tblPERSON WHERE Email = @Email)
+			BEGIN
+				INSERT INTO tblPERSON(Fname, Lname, Email)
+				VALUES(@F, @L, @Email)
+			END
 
-		-- add person info to tblPERSON if new entry
-		IF NOT EXISTS (
-			SELECT PersonID FROM tblPERSON
-				WHERE email = @Email
-		)
-		BEGIN
-			INSERT INTO tblPERSON(Fname, Lname, Email)
-			VALUES(@F, @L, @Email)
-		END
+		SET @PersonPK = SCOPE_IDENTITY()
+
+		INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+		VALUES(@PersonPK, @ResponseDateTime, @Email)
 
 		/* 
 		
@@ -57,6 +60,136 @@ BEGIN
 
 		*/
 
+		/* Q6 */
+
+		DECLARE @Q6 varchar(max) = (SELECT Question_6 FROM WK_1 WHERE ResponseID = @RowNum)
+
+
+		IF @Q6 = 'None of the above'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'None of the above')
+			END
+		ELSE
+			BEGIN
+				IF @Q6 LIKE '%participate in one or more Registered Student Organization%'
+					BEGIN
+						INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+						VALUES (@PersonPK, @ResponseDateTime, 'I participate in one or more Registered Student Organization(s) on campus')
+					END
+				IF @Q6 LIKE '%officer%'
+					BEGIN
+						INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+						VALUES (@PersonPK, @ResponseDateTime, 'I am an officer in one or more Registered Student Organization(s) on campus')
+					END
+				IF @Q6 LIKE '%sports%'
+					BEGIN
+						INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+						VALUES (@PersonPK, @ResponseDateTime, 'I participate in one or more sports teams (varsity or club)')
+					END
+				IF @Q6 LIKE '%Greek%'
+					BEGIN
+						INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+						VALUES (@PersonPK, @ResponseDateTime, 'I am in the Greek System')
+					END
+				IF @Q6 LIKE '%Fraternity%'
+					BEGIN
+						INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+						VALUES (@PersonPK, @ResponseDateTime, 'I am in a major-related Fraternity (i.e. Professional, Academic, etc.)')
+					END
+
+				/* IF [user typed response] */
+			END
+		
+		
+		/* Q7 */
+		DECLARE @Q7 varchar(max) = (SELECT Question_7 FROM WK_1 WHERE ResponseID = @RowNum)
+
+		IF @Q7 = 'Meet new people'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Meet new people')
+			END
+		IF @Q7 LIKE '%my major%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Get into my major')
+			END
+		IF @Q7 LIKE 'Join a club%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Join a club or RSO')
+			END
+		IF @Q7 LIKE 'Join a fraternity%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Join a fraternity or sorority')
+			END
+		IF @Q7 LIKE '%adult%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Learn how to be an adult (i.e. budgeting, time-management, cooking, responsibility)')
+			END
+		IF @Q7 LIKE '%connections%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Make connections with professors')
+			END
+		IF @Q7 LIKE '%Ph.D.%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Get into a Master''s or Ph.D. program')
+			END
+		IF @Q7 LIKE '%research%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Doing research as an undergrad')
+			END
+		IF @Q7 LIKE '%figure out%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Figure out what you want to do with your life')
+			END
+		IF @Q7 LIKE '%partner%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Find a partner')
+			END
+		IF @Q7 LIKE '%party%'
+			BEGIN
+				INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+				VALUES (@PersonPK, @ResponseDateTime, 'Go to a party')
+			END
+		/* IF [user typed response] */
+
+
+		/* Q 28-32 */
+		Declare @Response_28 varchar(100), 
+		@Response_29 varchar(100), @Response_30 varchar(100), @Response_31  varchar(100), 
+		@Response_32 varchar(100) -- declare variables
+
+		Set @Response_28 = (Select Question_28 From WK_1 Where ResponseID = @RowNum)
+		Set @Response_29 = (Select Question_29 From WK_1 Where ResponseID = @RowNum)
+		Set @Response_30 = (Select Question_30 From WK_1 Where ResponseID = @RowNum)
+		Set @Response_31 = (Select Question_31 From WK_1 Where ResponseID = @RowNum)
+		Set @Response_32 = (Select Question_32 From WK_1 Where ResponseID = @RowNum)
+	
+		Insert Into tblResponse(PersonID, ResponseDateTime, ResponseName)
+		Values(@PersonPK, @ResponseDateTime, @Response_28)
+		Insert Into tblResponse(PersonID, ResponseDateTime, ResponseName)
+		Values(@PersonPK, @ResponseDateTime, @Response_29)
+		Insert Into tblResponse(PersonID, ResponseDateTime, ResponseName)
+		Values(@PersonPK, @ResponseDateTime, @Response_30)
+		Insert Into tblResponse(PersonID, ResponseDateTime, ResponseName)
+		Values(@PersonPK, @ResponseDateTime, @Response_31)
+		Insert Into tblResponse(PersonID, ResponseDateTime, ResponseName)
+		Values(@PersonPK, @ResponseDateTime, @Response_32)
+
+
+		/* Q33 */
+		INSERT INTO tblRESPONSE(PersonID, ResponseDateTime, ResponseName)
+		VALUES (@PersonPK, @ResponseDateTime, @Temp)
+
 		SET @RowNum = @RowNum + 1
 
 	END
@@ -65,4 +198,4 @@ END
 
 
 -- sample exec
-EXEC uspInsertFromCSV @LastPull = '2020-05-22 12:00:00'
+EXEC uspInsertFromCSV @SurveyName = 'DogPaws Interest Survey', @LastPull = '2020-05-22 12:00:00'
