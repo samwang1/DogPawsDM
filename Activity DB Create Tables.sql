e03581a46779a26067728d74cf7cda833d9744d9
Chloe Tsien <chloet127@gmail.com>
2020-08-02 17:26:22 -0700

    create table statements for section one

--- Activity DB Create Tables.sql
+++ Activity DB Create Tables.sql
@@ -1,3 +1,22 @@
+ -- section 1
+CREATE TABLE tblCHARACTERISTIC (
+	CharID INT primary key identity(1,1),
+	CharName varchar(500) NULL,
+	CharDescr varchar(500) NULL
+)
+
+CREATE TABLE tblPROFILE (
+	ProfileID INT primary key identity(1,1),
+	ProfName varchar(500) NULL,
+	ProfDescr varchar(500) NULL
+)
+
+CREATE TABLE tblPROFILE_CHARACTERISTIC (
+	ProfCharID INT primary key identity(1,1),
+	CharID INT foreign key references tblCHARACTERISTIC(CharID) NOT NULL,
+	ProfID INT foreign key references tblPROFILE(ProfileID) NOT NULL
+)
+
  -- section 2
 Create Table tblBUILDING(
 	BuildingID int Primary Key Identity Not Null,
